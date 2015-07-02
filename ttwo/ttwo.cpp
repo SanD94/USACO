af3f0e4d0bb892f887ec6a08df002929bc65035e
andacsafa <andac.safa@gmail.com>
2015-07-02 11:21:32 +0300

    ttwo completed

--- ttwo/ttwo.cpp
+++ ttwo/ttwo.cpp
@@ -22,6 +22,45 @@ string map[11];
 int dir[4][2] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
 bool mark[401][401];
 
+bool check(ii &p, int *dd){
+    int x = p.fi + dd[0];
+    int y = p.se + dd[1];
+    if(0 <= x && x <10 &&
+            0 <= y && y < 10 &&
+            map[x][y] != '*') {
+        p.fi += dd[0];
+        p.se += dd[1];
+        return 1;
+    }
+    else return 0;
+}
+
+
+int go(ii &p, int dd){
+    if(check(p, dir[dd])) return dd;
+    else return (dd+1)%4;
+}
+
+
+int solve(){
+    int cdir = 0, fdir = 0;
+    int cloc = (cdir * 100) + (cows.fi*10 + cows.se);
+    int floc = (fdir * 100) + (farmer.fi*10 + farmer.se);
+    int res = 0;
+    while(!mark[floc][cloc]){
+        if(farmer.fi == cows.fi &&
+                farmer.se == cows.se) return res;
+        mark[floc][cloc] = 1;
+        cdir = go(cows,cdir);
+        fdir = go(farmer,fdir);
+        cloc = (cdir * 100) + (cows.fi*10 + cows.se);
+        floc = (fdir * 100) + (farmer.fi*10 + farmer.se);
+        res++;
+    }
+    return 0;
+}
+
+
 int main(){
     for(int i=0;i<10;i++){
         fin >> map[i];
@@ -31,7 +70,7 @@ int main(){
         }
     }
 
-
+    fout << solve() << endl;
 
     return 0;
 }
