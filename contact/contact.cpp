798aff659638cb792b336407feef2e72e33e76e8
andacsafa <andac.safa@gmail.com>
2015-07-07 14:00:08 +0300

    contact completed

---
+++ contact/contact.cpp
@@ -0,0 +1,94 @@
+/*
+ID: safaand1
+PROG: contact
+LANG: C++11
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <queue>
+#include <string>
+using namespace std;
+
+#define MAXN 200010
+#define fi first
+#define se second.first
+#define th second.second
+
+ifstream fin("contact.in");
+ofstream fout("contact.out");
+
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+
+int A,B,N;
+int parts[13][10000];
+char code[MAXN];
+char *p;
+priority_queue<iii> Q;
+
+int convert(int s,int len){
+    int e = s+len-1;
+    int res = 0;
+    while(e>=s) {
+        res<<=1;
+        res+= code[s++]-'0';
+    }
+    return res;
+}
+
+
+void add(int start,int len){
+    int num = convert(start,len);
+    parts[len][num]++;
+}
+
+
+string itos(int num,int len){
+    if(!len) return "";
+    char x = (num%2)+'0';
+    string k;
+    k.push_back(x);
+    return itos(num/2,len-1)+k;
+}
+
+int main(){
+    fin >> A >> B >> N;
+    p = code;
+    while(fin.good()) {fin >> p; p+=80;}
+    int len = strlen(code);
+    for(int i=0;i<len;i++)
+        for(int k=A;k<=B && i-k+1 >= 0; k++)
+            add(i-k+1,k);
+
+    for(int i=A;i<=B;i++){
+        int go = 1 << (i+1);
+        for(int j=0;j<go;j++)
+            if(parts[i][j])
+                Q.push(make_pair(parts[i][j], make_pair(-i,-j)));
+    }
+
+    
+    int gon = 0;
+    while(gon<N && !Q.empty()){
+        iii tp = Q.top(); Q.pop();
+        fout << tp.fi  << endl;
+        fout << itos(-tp.th,-tp.se);
+        int line = 1;
+        while(!Q.empty() && Q.top().fi == tp.fi){
+            if(line==6) { fout << endl; line=0;}
+            else fout << " "; 
+
+            tp = Q.top(); Q.pop();
+            fout << itos(-tp.th,-tp.se);
+            line++;
+        }
+       
+        fout << endl;
+        gon++;
+    }
+
+    return 0;
+}
