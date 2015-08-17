4b7a90776251dfc0f4c1c9e815add47089494cfa
andacsafa <andac.safa@gmail.com>
2015-08-17 16:52:56 +0300

    camelot completed

--- camelot/camelot.cpp
+++ camelot/camelot.cpp
@@ -7,10 +7,12 @@ LANG: C++11
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 #define mp make_pair
+#define pb push_back
 #define fi first
 #define se second
 #define MAXX 1000000
@@ -19,19 +21,20 @@ using namespace std;
 #define min(a,b) (a)<(b)?(a):(b)
 #define abs(x) (x)>0?(x):-(x)
 typedef pair<int,int> ii;
-typedef pair<ii,int> iii;
+typedef pair<ii, int> iii;
 
 ifstream fin("camelot.in");
 ofstream fout("camelot.out");
 
 int c,r;
-iii min_knight[26][30];
-int sum_move[26][30];
-bool knights[26][30];
+int sum_move[30][30];
+bool knights[30][30];
+vector<ii> n_places;
 ii theking;
-int steps[26][30][26][30];
+int steps[30][30][30][30];
 char column;
-int row, res;
+int row;
+int res;
 ii moves[8] = { mp(2,1), mp(2,-1), mp(-2,1), mp(-2,-1),
     mp(1,2), mp(1,-2), mp(-1,2), mp(-1,-2) };
 ii k_moves[8] = { mp(1,0), mp(0,1), mp(-1,0), mp(0,-1),
@@ -46,26 +49,26 @@ bool boundary_check(ii p){
 
 
 void eval_sum(ii start){
-    bool mark[26][30] = {};
+    bool mark[30][30] = {};
     mark[start.fi][start.se] = 1;
     Q.push(mp(start,0));
     while(!Q.empty()){
-	ii now = Q.front().fi;
-	int step = Q.front().se;
-	Q.pop();
-	if(knights[now.fi][now.se]) {
-	    if(min_knight[start.fi][start.se].se == MAXX)
-		min_knight[start.fi][start.se] = mp(now,step);
-	    sum_move[start.fi][start.se] += step;
-	}
-	steps[start.fi][start.se][now.fi][now.se] = step;
-	for(int i=0;i<8;i++){
-	    ii next = mp(now.fi + moves[i].fi, now.se + moves[i].se);
-	    if(boundary_check(next) && !mark[next.fi][next.se]){
-		mark[next.fi][next.se] = 1;
-		Q.push(mp(next,step+1));
-	    }
-	}
+        ii now = Q.front().fi;
+        int step = Q.front().se;
+        Q.pop();
+        if(knights[now.fi][now.se]){
+            if(sum_move[start.fi][start.se] == MAXX) 
+                sum_move[start.fi][start.se] = 0; 
+            sum_move[start.fi][start.se] += step;
+        }
+        steps[start.fi][start.se][now.fi][now.se] = step;
+        for(int i=0;i<8;i++){
+            ii next = mp(now.fi + moves[i].fi, now.se + moves[i].se);
+            if(boundary_check(next) && !mark[next.fi][next.se]){
+                mark[next.fi][next.se] = 1;
+                Q.push(mp(next,step+1));
+            }
+        }
     }
 }
 
@@ -74,49 +77,72 @@ int go_alone(ii dest){
 }
 
 void eval_kings_move(ii dest){
+    if(sum_move[dest.fi][dest.se] >= res) return;
     int mx_move = go_alone(dest);
-    bool mark[26][30] = {};
+    bool mark[30][30] = {};
+    int dec_step = MAXX;
     mark[theking.fi][theking.se] = 1;
     Q.push(mp(theking,0));
+    
     while(!Q.empty()){
-	ii now = Q.front().fi;
-	int step = Q.front().se;
-	Q.pop();
-	if(step > mx_move) continue;
-	//eval...
-	for(int i=0;i<8;i++){
-	    ii next = mp( now.fi + k_moves[i].fi, now.se + k_moves[i].se);
-	    if( boundary_check(next) && !mark[next.fi][next.se]){
-		Q.push(mp(next,step+1));
-		mark[next.fi][next.se] = 1;
-	    }
-	}
+        ii now = Q.front().fi;
+        int step = Q.front().se;
+        Q.pop();
+        if(step > mx_move) continue;
+    //    if(dec_step < steps[now.fi][now.se][dest.fi][dest.se] + step)
+      //      continue;
+        for(int i=0;i<(int)n_places.size();i++){
+            ii n_place = n_places[i];
+            dec_step = min(steps[n_place.fi][n_place.se][now.fi][now.se] +
+                steps[now.fi][now.se][dest.fi][dest.se] + step -
+                steps[n_place.fi][n_place.se][dest.fi][dest.se], dec_step);
+            res = min(res, dec_step+sum_move[dest.fi][dest.se]);
+        }
+        for(int i=0;i<8;i++){
+            ii next = mp( now.fi + k_moves[i].fi, now.se + k_moves[i].se);
+            if( boundary_check(next) && !mark[next.fi][next.se]){
+                Q.push(mp(next,step+1));
+                mark[next.fi][next.se] = 1;
+            }
+        }
 	
     }
 }
 
 void init(){
-    for(int i=0;i<26;i++)
-	for(int j=0;j<30;j++)
-	    min_knight[i][j].se = MAXX;
+    for(int i=0;i<30;i++)
+        for(int j=0;j<30;j++)
+            sum_move[i][j] = MAXX;
+    for(int i=0;i<30;i++)
+        for(int j=0;j<30;j++)
+            for(int k=0;k<30;k++)
+                for(int l=0;l<30;l++)
+                    steps[i][j][k][l] = MAXX;
     res = MAXX;
 }
 
 int main(){
     init();
-    fin >> c >> r;
+    fin >> r >> c;
     fin >> column >> row;
     theking = mp(to_num(column),row-1);
     while(fin.good()){
-	fin >> column >> row;
-	knights[to_num(column)][row-1] = 1;
+        fin >> column >> row;
+        knights[to_num(column)][row-1] = 1;
+        n_places.pb(mp(to_num(column), row-1));
     }
     for(int i=0;i<c;i++)
-	for(int j=0;j<r;j++)
-	    eval_sum(mp(i,j));
+        for(int j=0;j<r;j++)
+            eval_sum(mp(i,j));
+
+//    for(int i=0;i<c;i++){
+//        for(int j=0;j<r;j++)
+//            cout << sum_move[i][j] << " ";
+//        cout << endl;
+//    }
     for(int i=0;i<c;i++)
-	for(int j=0;j<r;j++)
-	    eval_kings_move(mp(i,j));
+        for(int j=0;j<r;j++)
+            eval_kings_move(mp(i,j));
     fout << res << endl;
     return 0;
 }
