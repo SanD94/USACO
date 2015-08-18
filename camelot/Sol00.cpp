5e4b96e1f5a4686781783c9a526d047f9c36855b
andacsafa <andac.safa@gmail.com>
2015-08-18 16:53:01 +0300

    camelot better solution added

--- camelot/Sol00.cpp
+++ camelot/Sol00.cpp
@@ -74,12 +74,12 @@ void calc_dist(int col, int row) {
     int max; 		/* maximum finite distance found so far */
     int f; 		/* temporary variable (returned value from do_step */
 
-/* initiate all positions to be infinite distance away */
+    /* initiate all positions to be infinite distance away */
     for (lv = 0; lv < ncol; lv++)
         for (lv2 = 0; lv2 < nrow; lv2++)
             dist[lv][lv2][0] = dist[lv][lv2][1] = MAXN;
 
-/* starting location is zero w/o king, kdist[col][row] with king */
+    /* starting location is zero w/o king, kdist[col][row] with king */
     dist[col][row][0] = 0;
     max = dist[col][row][1] = kdist[col][row];
 
@@ -118,35 +118,35 @@ int main(int argc, char **argv) {
     pc = t[0] - 'A';
     pr--;
 
-  /* Calculate cost of moving king from starting position to
-   * each board position.  This is just the taxi-cab distance */
-   for (lv = 0; lv < ncol; lv++)
-       for (lv2 = 0; lv2 < nrow; lv2++) {
-           i = abs(pc-lv);
-           j = abs(pr-lv2);
-           if (i < j) i = j;
-           kingcost[lv][lv2] = kdist[lv][lv2] = i;
-       }
+    /* Calculate cost of moving king from starting position to
+     * each board position.  This is just the taxi-cab distance */
+    for (lv = 0; lv < ncol; lv++)
+        for (lv2 = 0; lv2 < nrow; lv2++) {
+            i = abs(pc-lv);
+            j = abs(pr-lv2);
+            if (i < j) i = j;
+            kingcost[lv][lv2] = kdist[lv][lv2] = i;
+        }
 
-   while (fscanf (fin, "%s %d", t, &pr) == 2) { /* for all knights */
-       pc = t[0] - 'A';
-       pr--;
+    while (fscanf (fin, "%s %d", t, &pr) == 2) { /* for all knights */
+        pc = t[0] - 'A';
+        pr--;
 
-       /* calculate distances */
-       calc_dist(pc, pr);
+        /* calculate distances */
+        calc_dist(pc, pr);
 
-       for (lv = 0; lv < ncol; lv++)
-           for (lv2 = 0; lv2 < nrow; lv2++) {
-               /* to collect here, we must also move knight here */
-               cost[lv][lv2] += dist[lv][lv2][0];
+        for (lv = 0; lv < ncol; lv++)
+            for (lv2 = 0; lv2 < nrow; lv2++) {
+                /* to collect here, we must also move knight here */
+                cost[lv][lv2] += dist[lv][lv2][0];
 
-               /* check to see if it's cheaper for the new knight to
-                  pick the king up instead of whoever is doing it now */
-               if (dist[lv][lv2][1] - dist[lv][lv2][0] < kingcost[lv][lv2]) {
-                   kingcost[lv][lv2] = dist[lv][lv2][1] - dist[lv][lv2][0];
-               }
-           }
-   }
+                /* check to see if it's cheaper for the new knight to
+                   pick the king up instead of whoever is doing it now */
+                if (dist[lv][lv2][1] - dist[lv][lv2][0] < kingcost[lv][lv2]) {
+                    kingcost[lv][lv2] = dist[lv][lv2][1] - dist[lv][lv2][0];
+                }
+            }
+    }
     /* find best square to collect in */
     pc = cost[0][0] + kingcost[0][0];
 
@@ -154,7 +154,7 @@ int main(int argc, char **argv) {
         for (lv2 = 0; lv2 < nrow; lv2++)
             if (cost[lv][lv2] + kingcost[lv][lv2] < pc) /* better square? */
                 pc = cost[lv][lv2] + kingcost[lv][lv2]; 
-  fprintf (fout, "%i\n", pc);
-  return 0;
+    fprintf (fout, "%i\n", pc);
+    return 0;
 }
 
