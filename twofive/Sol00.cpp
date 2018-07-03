ab812372635eea029ba02b9fdcdba4aeb0f7b924
Safa ANDAC <sand94@Safas-MacBook-Pro.local>
2018-07-03 07:41:21 +0300

    twofive completed

--- twofive/Sol00.cpp
+++ twofive/Sol00.cpp
@@ -1,94 +1,99 @@
-#include <cstdio>
-#include <cstring>
-
-#define REPD( i, n ) \
-    for ( i = (n) - 1; i >= 0; i-- )
-
-const int MAXL = 26;
+#include <stdio.h>
+#include <string.h>
+#define MAX 5
+#define ALL 25
+using namespace std;
 
 int N;
-char W[MAXL];
-int dp[6][6][6][6][6];
-int s[6];
-int x[MAXL];
-int y[MAXL];
-bool used[MAXL];
+char mode; /* mode, we're looking for string or for a number */
+char str[ALL+1]; /* output string */
+int nways[MAX+1][MAX+1][MAX+1][MAX+1][MAX+1]; /* number of ways to fill the rest of the grid */
+int used[ALL]; /* is the letter used */
+int maxr[MAX]; /* the biggest letter in a row */
+int maxc[MAX]; /* the biggest letter in a column */
 
-int count() {
+/* calculate number of ways to fill the grid with 'a' letters in the 1st row, 'b' in the 2nd etc. */
+/* 'l' is the first letter we try to place */
+int calc(int a, int b, int c, int d, int e,int l) {
+    int cur = 0;
+    if( nways[a][b][c][d][e] )
+	return nways[a][b][c][d][e];
 
-    memset( dp, 0, sizeof( dp ) );
-    dp[5][5][5][5][5] = 1;
+    if( used[l] ) 
+	return nways[a][b][c][d][e] = calc(a,b,c,d,e,l+1);
 
-    s[0] = 5;
-    REPD( s[1], 6 )
-        REPD( s[2], 6 )
-        REPD( s[3], 6 )
-        REPD( s[4], 6 )
-        REPD( s[5], 6 ) {
+    if( a < 5 && l > maxr[0] && l > maxc[a] ) cur += calc(a+1,b,c,d,e,l+1);
+    if( a > b && l > maxr[1] && l > maxc[b] ) cur += calc(a,b+1,c,d,e,l+1);
+    if( b > c && l > maxr[2] && l > maxc[c] ) cur += calc(a,b,c+1,d,e,l+1);
+    if( c > d && l > maxr[3] && l > maxc[d] ) cur += calc(a,b,c,d+1,e,l+1);
+    if( d > e && l > maxr[4] && l > maxc[e] ) cur += calc(a,b,c,d,e+1,l+1);
 
-            int ch = s[1]+s[2]+s[3]+s[4]+s[5];
-            int &val = dp[s[1]][s[2]][s[3]][s[4]][s[5]];
+    return nways[a][b][c][d][e] = cur;
+}
+/* place new letter on the grid and prepare arrays for running calc() */
+void prepare( int letter, int r, int c ) {
+    memset( nways, 0, sizeof(nways) );
+    nways[5][5][5][5][5] = 1;
+    maxr[r] = maxc[c] = letter;
+    used[letter] = 1;
+}
+/* find the word from a number */
+void find_word( int N ) {
+    int l,a,cur=0,in[MAX]={0},tmp;
+    char str[ALL+1] = {0};
 
-            if ( used[ch] ) {
-                if ( s[ x[ch] ] < s[ x[ch] - 1 ] && s[ x[ch] ] + 1 == y[ch] ) {
-                    s[ x[ch] ]++;
-                    val = dp[s[1]][s[2]][s[3]][s[4]][s[5]];
-                    s[ x[ch] ]--;
-                }
-            } else
-                for ( int i = 1; i <= 5; i++ )
-                    if ( s[i] < s[i - 1] ) {
-                        s[i]++;
-                        val += dp[s[1]][s[2]][s[3]][s[4]][s[5]];
-                        s[i]--;
-                    }
-        }
+    memset( maxr, -1, sizeof(maxr) );
+    memset( maxc, -1, sizeof(maxc) );
+    memset( used,  0, sizeof(used) );
 
-    return dp[0][0][0][0][0];
+    for( a = 0; a < ALL; a++) {
+	in[a/5]++;
+	/* go to the next letter */
+	for( l = 0; l < ALL; l++ ) {
+	    if( !used[l] && l > maxr[a/5] && l > maxc[a%5] ) {
+		prepare( l, a/5, a%5 );
+
+		tmp = calc(in[0],in[1],in[2],in[3],in[4],0);
+
+		/* stop when too much was skipped */
+		if( cur + tmp >= N ) break;
+		cur += tmp;
+		used[l] = 0;
+	    }
+	}
+	str[a] = char(l+'A');
+    }
+    printf("%s\n", str);
+}
+/* find number of a grid */
+void find_num(char *str) {
+    int l,a,num=1,in[MAX]={0};
+
+    for( a = 0; a < ALL; a++ ) {
+	in[a/5]++;
+	for( l = 0; l < int(str[a]-'A'); l++ )
+	    if( !used[l] && l > maxr[a/5] && l > maxc[a%5] ) {
+		prepare( l, a/5, a%5 );
+		num += calc(in[0],in[1],in[2],in[3],in[4],0);
+		used[l] = 0;
+	    }
+	used[l] = 1; maxr[a/5] = maxc[a%5] = l;
+    }
+    printf("%d\n", num);
 }
 
 int main() {
+    freopen("twofive.in", "r", stdin);
+    freopen("twofive.out","w", stdout);
 
-    if ( getchar() == 'W' ) {
+    scanf("%c", &mode);
 
-        scanf( "%s", W );
-        for ( int i = 1; i < 6; i++ )
-            for ( int j = 1; j < 6; j++ )
-                for ( int k = 0; k < 25; k++ ) {
-                    if ( used[k] ) continue;
-                    used[k] = true;
-                    x[k] = i;
-                    y[k] = j;
-                    if ( W[(i-1)*5+j-1] == k + 'A' )
-                        break;
-                    N += count();
-                    used[k] = false;
-                }
-
-        printf( "%d\n", N + 1 );
-
-    } else {
-
-        scanf( "%d", &N ); N--;
-        for ( int i = 1; i < 6; i++ )
-            for ( int j = 1; j < 6; j++ )
-                for ( int k = 0; k < 25; k++ ) {
-                    if ( used[k] ) continue;
-                    used[k] = true;
-                    x[k] = i;
-                    y[k] = j;
-                    int cnt = count();
-                    if ( cnt > N ) {
-                        W[(i-1)*5+j-1] = k + 'A';
-                        break;
-                    }
-                    printf("%d\n", cnt);
-                    N -= cnt;
-                    used[k] = false;
-                }
-
-        printf( "%s\n", W );
+    if( mode == 'N' ) {
+	scanf("%d", &N);
+	find_word(N);
     }
-
-    return 0;
+    else {
+	scanf("%s", &str);
+	find_num(str);
+    }
 }
--- twofive/twofive.cpp
+++ twofive/twofive.cpp
@@ -1,79 +1,95 @@
 /*
 ID: safaand1
-PROG: twofive
 LANG: C++11
+PROG: twofive
 */
 
-#include <iostream>
+
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
+#define MAXL 25
 
-int count;
-int dyn[6][6][6][6][6];
+int count(bool used[], int x[], int y[]) {
+    int dp[6][6][6][6][6] = {};
+    int s[6];
+    dp[5][5][5][5][5] = 1;
+    s[0] = 5;
+    for(s[1] = 5; s[1] >= 0; s[1]--)
+    for(s[2] = 5; s[2] >= 0; s[2]--)
+    for(s[3] = 5; s[3] >= 0; s[3]--)
+    for(s[4] = 5; s[4] >= 0; s[4]--)
+    for(s[5] = 5; s[5] >= 0; s[5]--) {
+        int ch = s[1]+s[2]+s[3]+s[4]+s[5];
+        int &val = dp[s[1]][s[2]][s[3]][s[4]][s[5]];
+        if (used[ch]) {
+            if(s[x[ch]] < s[x[ch]-1] && s[x[ch]] + 1 == y[ch]) {
+                s[x[ch]]++;
+                val = dp[s[1]][s[2]][s[3]][s[4]][s[5]];
+                s[x[ch]]--;
+            }
+        } else
+            for (int i = 1; i <= 5; i++)
+                if (s[i] < s[i - 1]) {
+                    s[i]++;
+                    val += dp[s[1]][s[2]][s[3]][s[4]][s[5]];
+                    s[i]--;
+                }
+    }
 
-bool check(int a, int b, int c, int d, int e) {
-    if(a>5 || b>5 || c>5 || d>5 || e>5) return false;
-    if(a >= b && b >= c && c >= d && d >= e) return true;
-    return false;
+    return dp[0][0][0][0][0];
 }
 
-int eval(int a, int b, int c, int d, int e) {
-    if(!check(a,b,c,d,e)) return 0;
-    if(dyn[a][b][c][d][e]) return dyn[a][b][c][d][e];
-    dyn[a][b][c][d][e] = eval(a+1, b, c, d, e) + eval(a, b+1, c, d, e) +
-        eval(a, b, c+1, d, e) + eval(a, b, c, d+1, e) + eval(a, b, c, d, e+1);
-    return dyn[a][b][c][d][e];
+int find(string sequence) {
+    int res = 0;
+    bool used[MAXL] = {};
+    int x[MAXL] = {}, y[MAXL] = {};
+    for(int i=1;i<6;i++)
+    for(int j=1;j<6;j++)
+    for(int k=0;k<25;k++) {
+        if (used[k]) continue;
+        used[k] = true;
+        x[k] = i; y[k] = j;
+        if (sequence[(i-1)*5+j-1] == k + 'A') break;
+        res += count(used, x, y);
+        used[k] = false;
+    }
+    return res + 1;
 }
 
 string find(int num) {
-    string res(25,'A');
-    int arr[5] = {};
-    for(int i=0;i<25;i++)
-        for(int j=0;j<5;j++) {
-            arr[j]++;
-            if(check(arr[0], arr[1], arr[2], arr[3], arr[4])) {
-                int cur = dyn[arr[0]][arr[1]][arr[2]][arr[3]][arr[4]];
-                if (cur > num) { res[j*5+(arr[j]-1)] = 'A'+i; break; }
-                cout << cur << endl;
-                num -= cur;
-            }
-            arr[j]--;
+    string res(25, 'A');
+    bool used[MAXL] = {};
+    int x[MAXL] = {}, y[MAXL] = {};
+    for(int i=1;i<6;i++)
+    for(int j=1;j<6;j++)
+    for(int k=0;k<25;k++) {
+        if (used[k]) continue;
+        used[k] = true;
+        x[k] = i, y[k] = j;
+        int cnt = count(used,x,y);
+        if (cnt > num) {
+            res[(i-1)*5+j-1] = k + 'A';
+            break;
         }
-
+        num -= cnt;
+        used[k] = false;
+    }
     return res;
 }
 
 
-int find(string s) {
-    char cur = 'A';
-    int res = 0;
-    int arr[5] = { };
-    for(int i=0;i<25;i++)
-        for(int j=0;j<5;j++) {
-            arr[j]++;
-            if(check(arr[0], arr[1], arr[2], arr[3], arr[4])) {
-                if (s[j*5+arr[j]-1] == 'A'+i) break;
-                res += dyn[arr[0]][arr[1]][arr[2]][arr[3]][arr[4]];
-            }
-            arr[j]--;
-        }
-    return res + 1;
-}
-
-int main(int argc, char const* argv[]) {
-    //freopen("twofive.in", "r", stdin);
-    //freopen("twofive.out", "w", stdout);
+int main() {
+    freopen("twofive.in", "r", stdin);
+    freopen("twofive.out", "w", stdout);
     char type;
-    string seq;
+    string sequence;
     int num;
-
-    dyn[5][5][5][5][5] = 1;
-    eval(0,0,0,0,0);
-
     cin >> type;
     if (type == 'N') { cin >> num; cout << find(num-1) << endl; }
-    else { cin >> seq; cout << find(seq) << endl; }
+    else { cin >> sequence; cout << find(sequence) << endl; }
+    
     return 0;
 }
